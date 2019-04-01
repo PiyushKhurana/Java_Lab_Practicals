d1e4a642c76ed455c6975ebe6d95016a570854ad
PiyushKhurana <khuranapiyush27@gmail.com>
2019-04-01 17:26:33 +0530

    inital commit

---
+++ constructor_overloading.java
@@ -0,0 +1,55 @@
+ 
+class Box 
+{ 
+	int width, height, depth; 
+
+	Box(int w, int h, int d) 
+	{ 
+		width = w; 
+		height = h; 
+		depth = d; 
+	} 
+
+	 
+	Box() 
+	{ 
+		width = height = depth = 0; 
+	} 
+
+	
+	Box(int len) 
+	{ 
+		width = height = depth = len; 
+	} 
+ 
+	int volume() 
+	{ 
+		return width * height * depth; 
+	} 
+} 
+
+ 
+public class P 
+{ 
+	public static void main(String args[]) 
+	{  	Box mybox2 = new Box();
+		Box mybox3 = new Box(7); 
+		Box mybox1 = new Box(5,10,15); 
+		 
+		
+
+		int vol; 
+
+		
+		vol = mybox1.volume(); 
+		System.out.println(" Volume of mybox1 is " + vol); 
+
+		
+		vol = mybox2.volume(); 
+		System.out.println(" Volume of mybox2 is " + vol); 
+
+		 
+		vol = mybox3.volume(); 
+		System.out.println(" Volume of mybox3 is " + vol); 
+	} 
+} 
